d49fd480c3cf273ecc6b02762899f0de94362485
Yevgeniy Brikman <brikis98@users.noreply.github.com>
2026-04-09 15:39:39 +0100

    Create initial app code

---
+++ app/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', async (req, res) => {
+  res.render('hello');
+});
+
+module.exports = app;
