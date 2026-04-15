99f1ca87200c34a69e2e07e613db4f9a30cba2e6
Yevgeniy Brikman <brikis98@users.noreply.github.com>
2026-04-15 15:26:14 +0100

    Fix Grafana URL

--- tofu/live/bizcloud-ai/main.tf
+++ tofu/live/bizcloud-ai/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 provider "grafana" {
-  url  = module.managed_grafana.workspace_endpoint
+  url  = "https://${module.managed_grafana.workspace_endpoint}"
   auth = aws_grafana_workspace_service_account_token.dashboard_provisioner.key
 }
 
